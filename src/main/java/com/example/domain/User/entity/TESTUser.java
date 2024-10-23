bcc5ad56cef7cb395b22671f726d041121ddfec7
ujinyaa <jin81441@gmail.com>
2024-10-23 21:31:40 +0900

    Create TESTUser.java

---
+++ src/main/java/com/example/domain/User/entity/TESTUser.java
@@ -0,0 +1,19 @@
+package com.example.ggum.domain.user.entity;
+
+public class TESTUser {
+    public static User alice;  // 게시글 작성용 유저 데이터
+
+    public static void main(String[] args) {
+        // 유저 데이터 생성 (아이디는 데이터베이스에서 자동 생성, 비밀번호는 모두 1234*)
+        alice = new User(null, "1234*", "alice");  // Alice 유저 생성
+        User bob = new User(null, "1234*", "bob");
+        User charlie = new User(null, "1234*", "charlie");
+        User daisy = new User(null, "1234*", "daisy");
+
+        // 유저 데이터 출력 (테스트용)
+        System.out.println("User1 Username: " + alice.getUsername() + ", Password: " + alice.getPassword());
+        System.out.println("User2 Username: " + bob.getUsername() + ", Password: " + bob.getPassword());
+        System.out.println("User3 Username: " + charlie.getUsername() + ", Password: " + charlie.getPassword());
+        System.out.println("User4 Username: " + daisy.getUsername() + ", Password: " + daisy.getPassword());
+    }
+}
