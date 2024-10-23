06adb3877b02d5006016e64adc7f3bb559c0c471
ujinyaa <jin81441@gmail.com>
2024-10-23 20:34:03 +0900

    Update ResponseDTO.java

--- src/main/java/com/example/domain/Serach/dto/ResponseDTO.java
+++ src/main/java/com/example/domain/Serach/dto/ResponseDTO.java
@@ -1,1 +1,20 @@
 //검색에 응답하는 DTO
+
+package com.example.domain.Search.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+  
+public class ResponseDTO<T> {
+    private String error;
+    private T data;
+}
+  
