aa31a44aec14b3856ebeb0e215c8253f757fb6c3
ujinyaa <jin81441@gmail.com>
2024-10-23 21:37:17 +0900

    Update TESTUser.java

--- src/main/java/com/example/domain/User/entity/TESTUser.java
+++ src/main/java/com/example/domain/User/entity/TESTUser.java
@@ -4,8 +4,8 @@ public class TESTUser {
     public static User alice;  // 게시글 작성용 유저 데이터
 
     public static void main(String[] args) {
-        // 유저 데이터 생성 (아이디는 데이터베이스에서 자동 생성, 비밀번호는 모두 1234*)
-        alice = new User(null, "1234*", "alice");  // Alice 유저 생성
+        // 유저 데이터 생성 (테스트용, 비밀번호는 모두 1234*)
+        User alice = new User(null, "1234*", "alice");  // Alice 유저 생성
         User bob = new User(null, "1234*", "bob");
         User charlie = new User(null, "1234*", "charlie");
         User daisy = new User(null, "1234*", "daisy");
