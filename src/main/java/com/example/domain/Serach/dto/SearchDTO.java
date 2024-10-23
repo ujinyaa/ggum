9a3595dcc48eb92010be8bf9ecc9dda50b650143
ujinyaa <jin81441@gmail.com>
2024-10-23 20:32:51 +0900

    Update SearchDTO.java

--- src/main/java/com/example/domain/Serach/dto/SearchDTO.java
+++ src/main/java/com/example/domain/Serach/dto/SearchDTO.java
@@ -1,2 +1,26 @@
 
 // 검색에 사용되는 DTO
+package com.example.domain.Search.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchDTO {
+    private String selectedCategory;
+    private String keyword;
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO<T> {
+    private String error;
+    private T data;
+}
