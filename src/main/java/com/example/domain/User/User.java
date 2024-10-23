eea93d070c31f30a4d2eb8d23362c8a23edf1d24
ujinyaa <jin81441@gmail.com>
2024-10-23 20:32:10 +0900

    Update User.java

--- src/main/java/com/example/domain/User/User.java
+++ src/main/java/com/example/domain/User/User.java
@@ -1,1 +1,41 @@
 
+package com.example.ggum.domain.user.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints={@UniqueConstraint(columnNames="id")})
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String username;
+
+    @Column(nullable=false)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @Column(nullable=false)
+    private String role;
+
+    @Column(nullable=false)
+    private long likeCount;
+
+    @Column(nullable=false)
+    private long dislikeCount;
+}
