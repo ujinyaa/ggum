a50bed7f15a9dc751c4f5194c1f21a69654f5525
ujinyaa <jin81441@gmail.com>
2024-10-23 20:18:03 +0900

    Update PostCategory.java

--- src/main/java/com/example/domain/Post/PostCategory.java
+++ src/main/java/com/example/domain/Post/PostCategory.java
@@ -1,1 +1,53 @@
+package com.example.ggum.domain.post.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+//import com.example.ggum.domain.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Post{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="post_id")
+    private Long Id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String postCategory;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(name="participant_limit", nullable = false)
+    private Long participantLimit;
+
+    @Column(name="participant_count", nullable = false)
+    private Long participantCount;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name="end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PostStatus poststatus;
+}
