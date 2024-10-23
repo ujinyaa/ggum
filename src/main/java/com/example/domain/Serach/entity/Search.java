8722e037689ca1cb29ad480b1053fb702943eee4
ujinyaa <jin81441@gmail.com>
2024-10-23 20:30:59 +0900

    Update Search.java

--- src/main/java/com/example/domain/Serach/entity/Search.java
+++ src/main/java/com/example/domain/Serach/entity/Search.java
@@ -1,1 +1,25 @@
 // 검색에 사용할 카테고리+ 키워드 정보 담아두는  entity
+package com.example.domain.Search.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Search {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String selectedCategory;
+
+    @Column(nullable = false)
+    private String keyword;
+}
