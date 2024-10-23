f4b5a1d8e6d66a63b16c42911b137138f525b701
ujinyaa <jin81441@gmail.com>
2024-10-23 21:39:13 +0900

    Update and rename User1.java to User.java

---
+++ src/main/java/com/example/domain/User/User.java
@@ -0,0 +1,28 @@
+//테스트용으로 간단하게 구현
+package com.example.ggum.domain.user.entity;
+
+public class User {
+    private Long id;          // private 필드
+    private String password;  // private 필드
+    private String username;  // private 필드
+
+    // 생성자
+    public User(Long id, String password, String username) {
+        this.id = id;
+        this.password = password;
+        this.username = username;
+    }
+
+    // Getter 메서드
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
