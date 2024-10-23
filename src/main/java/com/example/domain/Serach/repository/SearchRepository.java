1876500a4b02d2559b21da42073d23c596699209
ujinyaa <jin81441@gmail.com>
2024-10-23 20:34:18 +0900

    Update SearchRepository.java

--- src/main/java/com/example/domain/Serach/repository/SearchRepository.java
+++ src/main/java/com/example/domain/Serach/repository/SearchRepository.java
@@ -1,1 +1,9 @@
 
+package com.example.domain.Search.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.domain.Search.entity.Search;
+
+public interface SearchRepository extends JpaRepository<Search, Long> {
+    // 필요한 경우 커스텀 쿼리 메서드 추가
+}
